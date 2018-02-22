e72842451049aaa0e8bf41f856b142a5d6391835
danrigg11 <dan_rigg11@hotmail.com>
2018-02-22 14:41:58 +0000

    Fisrt upload
    first upload with bookshelf page and the boy who cried wolf page with images and sounds.

---
+++ TheBookShef/script.js
@@ -0,0 +1,87 @@
+function page_exit() {
+  document.getElementById('page1').className = 'open-card';
+}
+
+function page_1_2() {
+  document.getElementById('page1').className = 'open-card';
+}
+
+function page_2_1() {
+  document.getElementById('page1').className = 'close-card';
+}
+
+function page_2_3() {
+  document.getElementById('page2').className = 'open-card';
+}
+
+function page_3_2() {
+  document.getElementById('page2').className = 'close-card';
+}
+
+function page_3_4() {
+  document.getElementById('page3').className = 'open-card';
+}
+
+function page_4_3() {
+  document.getElementById('page3').className = 'close-card';
+}
+
+function page_4_5() {
+  document.getElementById('page4').className = 'open-card';
+}
+
+function page_5_4() {
+  document.getElementById('page4').className = 'close-card';
+}
+
+function page_5_6() {
+  document.getElementById('page5').className = 'open-card';
+}
+
+function page_6_5() {
+  document.getElementById('page5').className = 'close-card';
+}
+
+function page_6_7() {
+  document.getElementById('page6').className = 'open-card';
+}
+
+function page_7_6() {
+  document.getElementById('page6').className = 'close-card';
+}
+
+function page_7_8() {
+  document.getElementById('page7').className = 'open-card';
+}
+
+function page_8_7() {
+  document.getElementById('page7').className = 'close-card';
+}
+
+function page_8_9() {
+  document.getElementById('page8').className = 'open-card';
+}
+
+function page_9_8() {
+  document.getElementById('page8').className = 'close-card';
+}
+
+function page_9_10() {
+  document.getElementById('page9').className = 'open-card';
+}
+
+function page_10_9() {
+  document.getElementById('page9').className = 'close-card';
+}
+
+function page_10_11() {
+  document.getElementById('page10').className = 'open-card';
+}
+
+function page_11_10() {
+  document.getElementById('page10').className = 'close-card';
+}
+
+function page_11_12() {
+  document.getElementById('page11').className = 'open-card';
+}
